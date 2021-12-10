8da8a2528c8b16ec13bd457c1a48a8bcda34d01c
miqmiq <miquel.miravet@uv.es>
2021-12-10 09:47:26 -0800

    Script plot uploaded

--- wrappy/PlotsDensPhi.py
+++ wrappy/PlotsDensPhi.py
@@ -1,17 +1,9 @@
-'''
- *    python scritp reproducing the 2-D existence plots in arXiv:2006.08583
- *    for fermion-axion star. 
- *
- *    RUN: python3 PlotsDensPhi.py value_for_fa
- *
-'''
-
 from matplotlib import pyplot as plt
 import numpy as np
 from matplotlib import rc
 rc('text', usetex=True)
 rc('font',family='serif')
-import glob, os, sys, math
+import glob, os
 
 #=======================================================
 
@@ -52,34 +44,49 @@ def ReadFile(path):
 
 #===========================================================
 
-home_path = os.getcwd()
-path_exist = home_path+'/out/fa-'+sys.argv[1]+'/'
-path_plots = path_exist+'plots/'
-    
+fa = 10
+
+path = '/Users/miquelmiravet/Projects/IPAM_LA/axion-fermion-stars/fermion-axion-pywrap/wrappy/out/fa'+str(fa)+'/'
+path_mass = '/Users/miquelmiravet/Projects/IPAM_LA/axion-fermion-stars/fermion-axion-pywrap/mass_curves/results/'
+path_plots = '/Users/miquelmiravet/Projects/IPAM_LA/axion-fermion-stars/fermion-axion-pywrap/plots_fixed_mass/'
+
 try:
     os.makedirs(path_plots)
-except OSError as error:
-    print(error)
+except OSError:
+    pass
 
-fa = float(sys.argv[1])
 print('fa   = ',fa)
-print('\n log(fa) = ', math.log(fa,10))
 
-phic, rhoc, mass = ReadFile(path_exist)
-mass = mass[:,1:]
+phic, rhoc, mass = ReadFile(path)
+#mass = mass[:,1:]
 mass = np.swapaxes(mass,0,1)
-phic = phic[1:]
+#phic = phic[1:]
 
 fig, ax = plt.subplots(figsize = (12,8)) 
-c       = ax.pcolor(rhoc,phic, mass,cmap = 'bwr')
+c       = ax.pcolor(rhoc,phic, mass,cmap = 'coolwarm')
 cbar    = fig.colorbar(c, ax=ax)
 cbar.set_label(label = r'$M_{\rm T}\mu$', size = 16, weight = 'bold')
 
+for file in glob.glob(path_mass+"out_equalmass_*.txt"):
+    filename = os.path.basename(file)
+    mass = float(filename[14:-4]) 
+    print('Fixed mass value : ', mass)
+    data = np.loadtxt(file, skiprows = 3)
+    phim = data[:,0]
+    rhom = data[:,1]
+    if mass != 1.58608 or mass !=1.40618 :
+        ax.plot(rhom,phim,linestyle='dashed', linewidth = 3,  label = r'$M_T$ = '+str(mass))
+    
+ax.legend(loc = 'lower right',frameon = True, fontsize = 13)
 ax.set_xlabel(r'$\rho_c/\mu^2$', fontsize = 16)
 ax.set_ylabel(r'$\phi_c$', fontsize = 16)
-ax.set_title(r'$f_a = '+sys.argv[1]+'$', fontsize = 20, fontweight= 'bold')
-ax.set_ylim(top = 0.14)
+ax.set_xlim([0,0.01])
+ax.set_title(r'$f_a = $'+str(fa), fontsize = 20, fontweight= 'bold')
 ax.tick_params(axis = 'both', labelsize = 15)
 cbar.ax.tick_params(labelsize=15)
-    
-fig.savefig(path_plots+'Count_fa_'+sys.argv[1]+'_rho_vs_phi.png', dpi=400)
+
+fig.savefig(path_plots+'PlotMassCurves_fa'+str(fa)+'.pdf')
+ 
+#===========================================================
+
+
