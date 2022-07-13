4585483112b042498e373229deb8d0b553e28b92
simone <simone.albanesi@edu.unito.it>
2022-07-13 18:01:05 +0200

    minor

--- mass_curves/Examples/AnalyticalExample.cpp
+++ mass_curves/Examples/AnalyticalExample.cpp
@@ -38,15 +38,15 @@ int main(int argc, char *argv[]){
     //x_init          = 0.5; // curve that ends on boundary
     //y_init          = 0.5;
     //start_direction = 1;
-    x_init          = 0.3; // closed curve
-    y_init          = -1;
-    ds              = 0.02;  // used for initial step and for squares
+    x_init          = -0.6; // closed curve
+    y_init          = -0.30103;
+    ds              = 0.01;  // used for initial step and for squares
     xmin            = -1;
-    xmax            = 1;
-    ymin            = -1;
-    ymax            = 1;
+    xmax            = 0.2;
+    ymin            = -0.6;
+    ymax            = 0.6;
     tol             = 1e-10; // tolerance in bisection
-    maxpoints       = 100;   // maximum number of points for the level curve 
+    maxpoints       = 500;   // maximum number of points for the level curve 
 
     // Find the level curve and prin the ouput on "output.txt"
     FindLevelCurve(x_init, y_init, ds, start_direction, tol, MyFunction, xmin, xmax, ymin, ymax, fname, maxpoints);
